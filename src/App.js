10a0a5bb808de313cbd74a85e692ef51c41d38c7
rooo5 <rg96200480@gmail.com>
2025-07-09 16:15:02 +0530

    added new functions

--- src/App.js
+++ src/App.js
@@ -1,34 +1,67 @@
 import './App.css';
 import { useState } from 'react'
+import ShowTask from "./ShowTask";
 
 function App() {
   const [task, setTaks] = useState([]);
   const [newTask, setNewTaks] = useState('');
 
+  const clearInput = () => {
+    setNewTaks('')
+  }
   const handleClick = () => {
-    setTaks([...task, newTask]);
+    if (newTask.trim() === '') return;
+
+    const objTask = {
+      id: task.length === 0 ? 1 : task[task.length - 1].id + 1,
+      name: newTask,
+      completed: false,
+    }
+    setTaks([...task, objTask]);
+    clearInput();
+  }
+
+  const handleCompeleted = (id) => {
+    task.map((item) => {
+      if (item.id === id) {
+        item.completed = !item.completed;
+      }
+      return item;
+    })
+    setTaks([...task]);
   }
 
   const handleChange = (evt) => {
     setNewTaks(evt.target.value);
   }
 
-  const handleDelete = (newTaks) => {
-    setTaks(task.filter((item) => item !== newTaks));
+  const handleDelete = (id) => {
+    setTaks(task.filter((item) => item.id !== id));
   }
 
   return (
     <div className="App">
-      <div className='addTasks'></div>
-      <input type='string' onChange={handleChange} />
-      <button className='add' onClick={handleClick}>Add Task</button>
+      <div className='header'>
+        <h1>To Do List</h1>
+      </div>
+      <div className='addTasks'>
+        <input
+          id='input-bar'
+          type='string'
+          value={newTask}
+          onChange={handleChange}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              handleClick();
+            }
+          }}
+          placeholder='Enter the task' />
+        <button className='add' onClick={handleClick} >Add</button>
+      </div>
       <div className='list'>{
-        task.map((name, key) => {
+        task.map((task, key) => {
           return (
-            <div key={key}>
-              {name}
-              <button onClick={() => handleDelete(name)}>X</button>
-            </div>
+            <ShowTask key={key} name={task.name} id={task.id} completed={task.completed} handleDelete={handleDelete} handleCompeleted={handleCompeleted} />
           )
         })
       }</div>
